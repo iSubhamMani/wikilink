1cde4128b1058450d3ed32c678829601e63e0dc7
Subham Mani <manisubham09@gmail.com>
2026-02-17 19:51:07 +0530

    Add initial implementation of document indexing with Redis integration

---
+++ indexer/app.py
@@ -0,0 +1,68 @@
+from upstash_redis import Redis
+import re
+from pymongo import MongoClient
+from collections import Counter
+from nltk.corpus import stopwords
+import nltk
+import os
+from dotenv import load_dotenv
+from bson.objectid import ObjectId
+
+nltk.download('stopwords')
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client.get_database()
+docs = db["docs"]
+
+redis = Redis.from_env()
+
+stop_words = set(stopwords.words('english'))
+
+def preprocess(text):
+    text = text.lower()
+    words = re.findall(r'\b[a-z]+\b', text)
+
+    processed = []
+
+    for word in words:
+        if word not in stop_words:
+            processed.append(word)
+
+    return processed
+
+def index_doc(doc):
+    doc_id = str(doc["_id"])
+    text = doc.get("clean_text", "")
+
+    tokens = preprocess(text)
+
+    if not tokens:
+        return
+    
+    term_counts = Counter(tokens)
+    content_length = len(tokens)
+
+    redis.hset(f"doc_clen:{doc_id}", "clen", str(content_length))
+
+    for term, tf in term_counts.items():
+        redis.hset(f"term:{term}", doc_id, tf)
+        redis.incr(f"df:{term}")
+        print(f"Indexed term '{term}' for doc {doc_id} with tf={tf}")
+
+    redis.incr("stats:total_docs")
+    print(f"Indexed doc {doc_id} - {doc.get('title', '')}")
+
+if __name__ == "__main__":
+    while True:
+        doc_id = str(redis.rpop("wikidocs_queue"))
+        if doc_id is None:
+            continue
+        
+        doc = docs.find_one({
+            "_id": ObjectId(doc_id)
+        })
+        if doc:
+            index_doc(doc)
+    redis.flushdb()
