3e2dbf24e3443e5c08e85caf736f80525bbb54e2
Subham Mani <manisubham09@gmail.com>
2026-03-07 20:19:10 +0530

    Refactor SearchBox component to accept default search value and implement search functionality

--- app/components/SearchBox.tsx
+++ app/components/SearchBox.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-const SearchBox = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+const SearchBox = ({ defaultVal }: { defaultVal?: string }) => {
+  const [searchQuery, setSearchQuery] = useState<string>(defaultVal || "");
+  const router = useRouter();
 
   const handleSearch = (e: React.FormEvent) => {
     e.preventDefault();
-    // Implement search functionality here
+    if (searchQuery.trim() === "") return;
+    router.push(`/search?q=${encodeURIComponent(searchQuery.trim())}`);
   };
 
   return (
@@ -20,7 +23,7 @@ const SearchBox = () => {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           placeholder="Search Wikipedia..."
-          className="playfair w-full rounded-full border-2 border-neutral-400 text-neutral-800 placeholder-neutral-500 py-3 pl-10 pr-4 text-sm sm:text-lg focus:outline-none focus:border-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-600 transition-all"
+          className="playfair w-full rounded-full border-2 border-neutral-400 text-neutral-800 placeholder-neutral-500 py-3 pl-10 pr-4 text-sm sm:text-base focus:outline-none focus:border-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-600 transition-all"
           aria-label="Wikipedia search input"
         />
       </div>
